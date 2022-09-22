ff2b69bb1987e12878796c4f4a03fa7f32eab683
IvanGlaz79vvv <ivangl79@gmail.com>
2022-09-23 00:48:50 +0300

    Hibernate_Test

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,23 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
+public class Main {
+    public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Гарик", "Харламов", (byte) 39);
+        userService.saveUser("Павел", "Воля", (byte) 41);
+        userService.saveUser("Руслан", "Белый", (byte) 40);
+        userService.saveUser("Гарик", "Мартиросян", (byte) 46);
+
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
+}
